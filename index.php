4b61c12eaa78f03357c42b1534ad8812fa80b097
sumit-snap <39926357+sumit-snap@users.noreply.github.com>
2018-08-11 14:57:15 +0530

    login page

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+	<title>Login</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+	<link rel="stylesheet" type="text/css" href="styles.css">
+</head>
+<body>
+<?php
+	echo"<div class='container' style='border:1px solid #d8d8d8'>";
+		echo"<h1>Please Login First</h1>";
+		echo"<h4>Username</h4>";
+		echo"<input type='text' class='form-control' placeholder='Enter Username'>";
+		echo"<h4>Password</h4>";
+		echo"<input type='password' class='form-control' placeholder='Enter Password'><br/>";
+		echo"<button type='button' class='btn btn-default'>Login</button><br/><br/>";
+    echo"</div>";
+    ?>
+</body>
+</html>
